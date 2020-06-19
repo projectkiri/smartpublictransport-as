15696bf5bf5cc28922d21a851d076c0afd205c60
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2020-06-19 16:26:56 +0700

    Set project home to https://projectkiri.id
    Upgrade Gradle and bump version

--- app/src/main/java/travel/kiri/smarttransportapp/model/protocol/CicaheumLedengProtocol.java
+++ app/src/main/java/travel/kiri/smarttransportapp/model/protocol/CicaheumLedengProtocol.java
@@ -43,7 +43,7 @@ import javax.net.ssl.HttpsURLConnection;
 public class CicaheumLedengProtocol {
 	private final String apiKey;
 
-	private static final String KIRI_HOSTNAME = "https://kiri.travel";
+	private static final String KIRI_HOSTNAME = "https://projectkiri.id";
 	private static final String KIRI_HANDLE_URL = KIRI_HOSTNAME + "/api";
 	private static final String KIRI_ICONS_PATH = KIRI_HOSTNAME + "/images/means";
 	private static final String KIRI_BALOON_START_URL = KIRI_HOSTNAME + "/images/stepicon-walkstart.png";
